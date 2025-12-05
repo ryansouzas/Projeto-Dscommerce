07d7b41f7ebd16be2ad83fac3f9ab17a8c62260a
Ryan Souza <ryanssebadelhe@gmail.com>
2025-12-05 11:33:19 -0300

    DELETE UPDATE product

--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -30,11 +30,31 @@ public class ProductService {
     @Transactional
     public ProductDTO insert(ProductDTO dto){
         Product entity = new Product();
+        copyDtoToEntity(dto,entity);
+        entity = repository.save(entity);
+        return new ProductDTO(entity);
+    }
+
+    @Transactional
+    public ProductDTO update(Long id, ProductDTO dto){
+        Product entity = repository.getReferenceById(id);
+        copyDtoToEntity(dto,entity);
+        return new ProductDTO(entity);
+    }
+
+    @Transactional
+    public void delete(Long id){
+        repository.deleteById(id);
+    }
+
+
+    private void copyDtoToEntity(ProductDTO dto, Product entity) {
         entity.setName(dto.getName());
         entity.setDescription(dto.getDescription());
         entity.setPrice(dto.getPrice());
         entity.setImgUrl(dto.getImgUrl());
-        entity = repository.save(entity);
-        return new ProductDTO(entity);
     }
+
+
+
 }
