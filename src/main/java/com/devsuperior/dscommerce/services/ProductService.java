9b3149c5e46f49b45a4a3ecceb51c60465cdfbfa
Ryan Souza <ryanssebadelhe@gmail.com>
2026-01-12 10:54:02 -0300

    ProductMinDTO

--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dscommerce.services;
 
 import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.dto.ProductMinDTO;
 import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.repositories.ProductRepository;
 import com.devsuperior.dscommerce.services.exceptions.DataBaseException;
@@ -28,9 +29,9 @@ public class ProductService {
     }
 
     @Transactional(readOnly = true)
-    public Page<ProductDTO> findAll(String name, Pageable pageable){
+    public Page<ProductMinDTO> findAll(String name, Pageable pageable){
         Page<Product> list =  repository.searchByName(name, pageable);
-        return list.map(x -> new ProductDTO(x));
+        return list.map(x -> new ProductMinDTO(x));
     }
 
     @Transactional
