fb7419458c6fdc1b5fb157ed492122bc12cb7707
Ryan Souza <ryanssebadelhe@gmail.com>
2026-06-09 21:27:02 -0300

    ProductControllerIT  -Obtaining access token

--- backend/src/test/java/com/devsuperior/dscommerce/controllers/it/ProductControllerIT.java
+++ backend/src/test/java/com/devsuperior/dscommerce/controllers/it/ProductControllerIT.java
@@ -1,6 +1,10 @@
 package com.devsuperior.dscommerce.controllers.it;
 
 
+import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.entities.Product;
+import com.devsuperior.dscommerce.tests.TokenUtil;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,12 +29,28 @@ public class ProductControllerIT {
     private MockMvc mockMvc;
     private String productName;
 
+    @Autowired
+    private ObjectMapper objectMapper;
 
-    private String adminToken;
+    private String clientUsername, clientPassword, adminUsername, adminPassword;
+
+    private TokenUtil tokenUtil;
+    private String clientToken, adminToken, invalidToken;
+
+    private Product product;
+    private ProductDTO productDTO;
 
     @BeforeEach
     void setUp() throws Exception{
+        clientUsername = "maria@gmail.com";
+        clientPassword = "123456";
+        adminUsername = "alex@gmail.com";
+        adminPassword = "123456";
         productName = "Macbook";
+
+        clientToken = tokenUtil.obtainAccessToken(mockMvc, clientUsername, clientPassword);
+        adminToken = tokenUtil.obtainAccessToken(mockMvc, adminUsername, adminPassword);
+        invalidToken = adminToken + "xpto";
     }
 
     @Test
@@ -59,5 +79,17 @@ public class ProductControllerIT {
         result.andExpect(jsonPath("$.content[0].imgUrl").value("https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/1-big.jpg"));
     }
 
+    @Test
+    public void insertShouldReturnProductDTOCreatedWhenAdminLogged() throws Exception {
 
+        String jsonBody = objectMapper.writeValueAsString(productDTO);
+
+        ResultActions result = mockMvc
+                .perform(post("/products")
+                        .header("Authorization", "Bearer " + adminToken)
+                        .content(jsonBody)
+                        .accept(MediaType.APPLICATION_JSON));
+
+
+    }
 }
